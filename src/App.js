eeac6b613b44d69a68d31b71e38ce7783e72b24b
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2021-11-05 09:19:07 +0100

    Settings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { Container } from '@mui/material';
+import Settings from './Settings';
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      grid: 5,
+      x: 0,
+      y: 0,
+      count: 1
+    };
+  }
+
+  handleChangeGrid = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  generatePositions = () => {
+
+  }
+
   render() {
     return (
-      <div>
-        ahoj
-      </div>
+      <Container className="mt-5">
+        <Settings handleChangeGrid={this.handleChangeGrid} grid={this.state.grid} x={this.state.x} y={this.state.y} count={this.state.count} />
+      </Container>
     )
   }
 }
---
+++ src/Settings.js
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default class Settings extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>Úvodné nastavenie</h2>
+        <Box sx={{ minWidth: 240 }}>
+          <FormControl variant="filled" sx={{ m: 1, minWidth: 240 }}>
+            <InputLabel id="gridLabel">Veľkosť mriežky</InputLabel>
+            <Select
+              labelId="gridLabel"
+              id="grid"
+              name="grid"
+              value={this.props.grid}
+              label="Veľkosť mriežky"
+              onChange={this.props.handleChangeGrid}
+            >
+              <MenuItem value={5}>5x5</MenuItem>
+              <MenuItem value={6}>6x6</MenuItem>
+              <MenuItem value={7}>7x7</MenuItem>
+              <MenuItem value={8}>8x8</MenuItem>
+            </Select>
+          </FormControl>
+        </Box >
+        <Box sx={{ minWidth: 240 }}>
+          <FormControl variant="filled" sx={{ m: 1, minWidth: 240 }}>
+            <InputLabel id="xLabel">Začiatočná pozícia x</InputLabel>
+            <Select
+              labelId="xLabel"
+              id="x"
+              name="x"
+              value={this.props.x}
+              label="Začiatočná pozícia x"
+              onChange={this.props.handleChangeGrid}
+            >
+              {[...Array(this.props.grid)].map((i, x) =>
+                <MenuItem key={x} value={x}>{x}</MenuItem>
+              )}
+            </Select>
+          </FormControl>
+        </Box >
+        <Box sx={{ minWidth: 240 }}>
+          <FormControl variant="filled" sx={{ m: 1, minWidth: 240 }}>
+            <InputLabel id="yLabel">Začiatočná pozícia y</InputLabel>
+            <Select
+              labelId="yLabel"
+              id="y"
+              name="y"
+              value={this.props.y}
+              label="Začiatočná pozícia y"
+              onChange={this.props.handleChangeGrid}
+            >
+              {[...Array(this.props.grid)].map((i, y) =>
+                <MenuItem key={y} value={y}>{y}</MenuItem>
+              )}
+            </Select>
+          </FormControl>
+        </Box >
+        <Box sx={{ minWidth: 240 }}>
+          <FormControl variant="filled" sx={{ m: 1, minWidth: 240 }}>
+            <InputLabel id="countLabel">Počet pokladov</InputLabel>
+            <Select
+              labelId="countLabel"
+              id="count"
+              name="count"
+              value={this.props.count}
+              label="Počet pokladov"
+              onChange={this.props.handleChangeGrid}
+            >
+              {[...Array(10)].map((i, y) =>
+                <MenuItem key={y} value={y + 1}>{y + 1}</MenuItem>
+              )}
+            </Select>
+          </FormControl>
+        </Box >
+        <Box sx={{ minWidth: 240 }}>
+          <div className="m-2">
+            <Button onClick={this.props.generatePositions} className="ml-5" size="small" variant="contained">Vygenuruj pozície pokladov</Button>
+          </div>
+        </Box>
+      </>
+    );
+  }
+}
